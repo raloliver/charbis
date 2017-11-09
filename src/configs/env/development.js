df9c39f6641897c15c425e0b82f3238ecabf14c5
Ral Oliver <raloliver.ws@gmail.com>
2017-11-08 23:54:14 -0200

    starts development env

---
+++ src/configs/env/development.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const expressSession = require('express-session')
+const expressValidator = require('express-validator')
+const bodyParser = require('body-parser')
+const hbs = require('express-hbs')
+
+module.exports = (app) => {
+    app.set('port', 9000)
+    app.set('host', '127.0.0.1')
+    app.set('views', path.join(__dirname, './../../../build/views'))
+    app.set('view engine', 'hbs')
+
+    // middlewares
+    app.use(morgan('dev'))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }))
+    app.use(methodOverride('_method'))
+    app.use(expressSession({
+        secret: 'v,MWZ=jVd4LuNgr]',
+        resave: false,
+        saveUninitialized: false
+    }))
+    app.use(expressValidator())
+
+    app.engine('hbs', hbs.express4({
+        defaultLayout: path.join(app.get('views', 'layout/main.hbs')),
+        partialsDir: path.join(app.get('views', 'partials')),
+        layoutsdDir: path.join(app.get('views', 'layouts'))
+    }))
+}
