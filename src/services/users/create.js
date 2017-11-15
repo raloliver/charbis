58d50ed1dd343aa9b8e5f6ac532dc35e1e35e954
Ral Oliver <raloliver.ws@gmail.com>
2017-11-15 19:25:58 -0200

    create a user

--- src/services/users/create.js
+++ src/services/users/create.js
@@ -1,3 +1,12 @@
+const Users = require('./../../schemas/users')
+
 module.exports = (req, res) => {
-    
+    Users
+        .create(req.body)
+        .then((user) => {
+            return res.redirect('/users')
+        })
+        .catch((error) => {
+            return res.send('Error ' + error)
+        })
 }
