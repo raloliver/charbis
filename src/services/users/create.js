7b20d6bd7ae43c05b473ed4043c7695f3e14c0f2
Ral Oliver <raloliver.ws@gmail.com>
2017-11-21 18:51:07 -0200

    create and list users

--- src/services/users/create.js
+++ src/services/users/create.js
@@ -1,6 +1,8 @@
 const Users = require('./../../schemas/users')
 
 module.exports = (req, res) => {
+    req.body.slug = req.body.name.toLowerCase().replace(/ /g, '-')
+
     Users
         .create(req.body)
         .then((user) => {
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,3 +1,15 @@
+const Users = require('./../../schemas/users')
+
 module.exports = (req, res) => {
-    
+    Users
+        .find()
+        .then((users) => {
+            return res.render('users/index', {
+                title: 'Users - charbis',
+                users: users //or just users
+            })
+        })
+        .catch((error) => {
+            return res.send('Error: ' + error)
+        })
 }
